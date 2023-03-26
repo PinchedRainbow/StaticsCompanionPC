0427e8ec95d5f2f6b1935d564af6673e5dc85736
Faheem Saleem <63436748+PinchedRainbow@users.noreply.github.com>
2023-03-26 23:43:35 +0100

    Added button to check for lockfile

---
+++ index.js
@@ -0,0 +1,39 @@
+import { join } from 'path';
+const fs = require('fs');
+
+let port, password, protocol, name, PID, accessToken, entitlementToken, lockFilePath;
+
+window.onload = () => {
+    let button = document.getElementById('ValoButton');
+    button.addEventListener('click', () => {
+        // check if the lockfile exists
+        alert('Checking if lockfile exists...');
+        let lockFileExists = readLockFile(getLockFilePath());
+        if (lockFileExists)
+        {
+            alert('Lockfile exists!');
+        }
+    });
+
+}
+
+// make a funcion that returns a file path
+function getLockFilePath() {
+    return join(process.env.LOCALAPPDATA, 'Riot Games', 'Riot Client', 'Config', 'lockfile');
+}
+
+function readLockFile(filePath) {
+    // read the lockfile
+    try{
+        const data = fs.readFileSync(getLockFilePath(), 'utf8').split(':');
+        name = data[0];
+        PID = data[1];
+        port = data[2];
+        password = data[3];
+        protocol = data[4];
+        return true;
+    }
+    catch(err) {
+        alert('Error reading lockfile: ' + err);
+    }
+}
