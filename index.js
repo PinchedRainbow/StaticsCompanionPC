2094c8f471cd5cb3b8346a75fff7ae99ad1b168b
Faheem Saleem <faheemusmi@gmail.com>
2023-03-27 00:00:43 +0100

    Fix js

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
-import { join } from 'path';
-const fs = require('fs');
+// import { join } from 'path';
+// const fs = require('fs');
 
 let port, password, protocol, name, PID, accessToken, entitlementToken, lockFilePath;
 
-window.onload = () => {
-    let button = document.getElementById('ValoButton');
-    button.addEventListener('click', () => {
-        // check if the lockfile exists
-        alert('Checking if lockfile exists...');
-        let lockFileExists = readLockFile(getLockFilePath());
-        if (lockFileExists)
-        {
-            alert('Lockfile exists!');
-        }
-    });
-
-}
-
 // make a funcion that returns a file path
 function getLockFilePath() {
-    return join(process.env.LOCALAPPDATA, 'Riot Games', 'Riot Client', 'Config', 'lockfile');
+    return process.env.LOCALAPPDATA + '\\Riot Games\\Riot Client\\Config\\lockfile';
 }
 
 function readLockFile(filePath) {
     // read the lockfile
     try{
-        const data = fs.readFileSync(getLockFilePath(), 'utf8').split(':');
+        const data = fs.readFileSync(filePath, 'utf8').split(':');
         name = data[0];
         PID = data[1];
         port = data[2];
@@ -35,5 +21,19 @@ function readLockFile(filePath) {
     }
     catch(err) {
         alert('Error reading lockfile: ' + err);
+        return false;
     }
 }
+
+// Wait for the window to load before adding the click event listener
+window.onload = () => {
+    let button = document.getElementById('ValoButton');
+    button.addEventListener('click', () => {
+        // check if the lockfile exists
+        alert('Checking if lockfile exists...');
+        let lockFileExists = readLockFile(getLockFilePath());
+        if (lockFileExists) {
+            alert('Lockfile exists!');
+        }
+    });
+};
